e11c3c3e6b887d17c46af2c7a2e021ced8d43a08
msharif9gsu <msharif9@student.gsu.edu>
2024-12-13 11:07:57 -0500

    modelComparison.py

--- modelComparison.py
+++ modelComparison.py
@@ -1,6 +1,5 @@
 # Compares the performance of Linear Regression, Random Forest,
 # Gradient Boosting, and Support Vector Regressor models.
-
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
